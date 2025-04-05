f1fcbc2b513641d4d133614c77a05e2f25b584ca
Ashish <ashishalex084@gmail.com>
2025-04-05 20:30:32 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FFCB05', // your goldish yellow
+    },
+    secondary: {
+      main: '#2C0A23', // maroon
+    },
+    background: {
+      default: '#110724', // dark background
+      paper: '#1c0e2e', // card/paper background
+    },
+    text: {
+      primary: '#ffffff',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )

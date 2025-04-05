613c1f28e9028489705382845415faa13212d331
githuber-boop <76819905+githuber-boop@users.noreply.github.com>
2025-04-05 14:19:57 +0000

    main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">User Management</Link>
+            <Link className="navbar-brand" to="/">User Management Dashboard</Link>
             <div className="collapse navbar-collapse">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item " >
